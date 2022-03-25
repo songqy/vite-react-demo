651026188af302c0231325fbaefdced60247a6c6
songqy <songqy3@126.com>
2022-03-25 15:37:09 +0800

    chore: eslint+prettier配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,24 +3,24 @@ module.exports = {
   extends: [
     require.resolve('@umijs/fabric/dist/eslint'),
   ],
-  globals: {
-    ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
-    page: true,
-    REACT_APP_ENV: true,
-  },
   ignorePatterns: ['.eslintrc.js', 'jest.config.js'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
     'no-plusplus': 0,
     'quotes': ['error', 'single'],
     'semi': ['error', 'always'],
-    'indent': ['error', 2],
-    'comma-dangle': ['error',  {
-      'arrays': 'always-multiline',
-      'objects': 'always-multiline',
-      'imports': 'always-multiline',
-      'exports': 'always-multiline',
-      'functions': 'never',
-    }],
+    // 'indent': ['error', 2],
+    // 'comma-dangle': ['error',  {
+    //   'arrays': 'always-multiline',
+    //   'objects': 'always-multiline',
+    //   'imports': 'always-multiline',
+    //   'exports': 'always-multiline',
+    //   'functions': 'never',
+    // }],
     'comma-spacing': 2, //强制在逗号周围使用空格
     'key-spacing': 2, // 在对象字面量的键和值之间使用一致的空格
     'keyword-spacing': 2, // 关键字周围空格的一致性
