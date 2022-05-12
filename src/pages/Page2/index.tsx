2f91c8984cd137c83d9ecf9379225efe12571ebc
songqy <songqy3@126.com>
2022-05-12 19:01:01 +0800

    feat: 修改

--- src/pages/Page2/index.tsx
+++ src/pages/Page2/index.tsx
@@ -1,15 +1,19 @@
-// import Plugin from '@c/plugin';
-import { NebulaApp } from '@nebulare/proxima';
+import { NebulaApp, ConfigProvider } from '@nebulare/proxima';
 
 const Page2 = () => {
   return (
     <div>
       <h3>page2</h3>
       <div>请求后端返回react结构</div>
-      <NebulaApp key="1" header="app1" />
-      <NebulaApp key="2" header="app2" />
-      {/* <Plugin name="Plugin AAAAA" />
-      <Plugin name="Plugin BBBBB" /> */}
+      <ConfigProvider faasApi="/api">
+        <NebulaApp
+          type="ItemPanel"
+          appId="app111"
+          faasApi="/api"
+          rootProps={{ header: 'app1' }}
+        />
+        <NebulaApp type="ItemPanel" rootProps={{ header: 'app2' }} />
+      </ConfigProvider>
     </div>
   );
 };
